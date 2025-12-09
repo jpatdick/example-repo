cab1f64385d2cf19954cf7379e329b70c6099a89
jpatdick <jerry.p.dickerson@gmail.com>
2025-12-08 19:31:21 -0500

    Remove readme.py

--- readme.py
+++
@@ -1,3 +0,0 @@
-# Project Name: album_management.py
-"""Description: This code creates two list of future albums and eventually combines them. 
-Additionally, it leverages a linear search and prints whether it was found and at what index."""
